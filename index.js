67a3f033c59534ac7dd05bcc2208ab1e00e7401b
MevanAhmed <mevanovic@gmail.com>
2022-08-06 12:45:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+
+const currentUser = 'Grace hopper';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
